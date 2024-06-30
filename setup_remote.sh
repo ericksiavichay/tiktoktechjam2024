1adb5d678eb58e966e12298a9913046d6f5d6d3b
Erick Siavichay <ericksiavichay40@gmail.com>
2024-06-29 22:24:35 -0500

    remote setup hotfix

--- setup_remote.sh
+++ setup_remote.sh
@@ -7,7 +7,7 @@ wget https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth
 # Upgrade pip
 pip install --upgrade pip
 
-pip install ignore-installed blinker
+pip install --ignore-installed blinker
 
 # Install Python dependencies from remote.txt
 pip install -r remote.txt

c26c923737b4d409708bdbd92413e61832ca4507
Erick Siavichay <ericksiavichay40@gmail.com>
2024-06-29 22:36:59 -0500

    added alternative ngrok download for runpod

--- setup_remote.sh
+++ setup_remote.sh
@@ -26,11 +26,9 @@ EOL
 
 mkdir movies
 
-curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | \
-  sudo gpg --dearmor -o /etc/apt/keyrings/ngrok.gpg && \
-  echo "deb [signed-by=/etc/apt/keyrings/ngrok.gpg] https://ngrok-agent.s3.amazonaws.com buster main" | \
-  sudo tee /etc/apt/sources.list.d/ngrok.list && \
-  sudo apt update && sudo apt install ngrok
+apt update
+apt install snap
+snap install ngrok
 
 # create ngrok auth token env variable
 echo "Please enter your ngrok auth token: "
